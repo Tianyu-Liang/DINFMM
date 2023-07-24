97b1dc8c1d691602fe494202f04a601969b07325
Tianyu-Liang <33270674+Tianyu-Liang@users.noreply.github.com>
2023-07-23 22:42:36 -0700

    Create parallel_example512.jl

---
+++ helmholtz/parallel_example512.jl
@@ -0,0 +1,14 @@
+
+t1 = time()
+using Distributed, ClusterManagers
+rmprocs(workers())
+addprocs(4) # this means to add 4 processes and factorize/solve in parallel
+@everywhere using LinearAlgebra
+@everywhere BLAS.set_num_threads(1)
+include("kernel_2D.jl")
+using .kernel_2D
+println("compile and addproc time: ", time() - t1)
+
+factorize_and_solve(128^2, 4; run_solve=false)
+factorize_and_solve(512^2, 6, 256; run_solve=false)
+
